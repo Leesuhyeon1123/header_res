4eccf89a33b712092cf0bb2ffb8440eb99aebd0a
lee su hyeon <qortjf3255@gmail.com>
2025-01-21 13:52:25 +0900

    pc nav, m nav 햄버거바 나오게하기 완료

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  // 햄버거바 클릭
   $("#hamburger-menu").click(function () {
     $(".mobile-nav").animate(
       {
@@ -7,13 +8,29 @@ $(document).ready(function () {
       300
     );
   });
-
-  $(".close-menu").click(function () {
+  // 모바일 gnb > menu(햄버거바) 호버했을때 submenu(depth2) 나오고 사라지게하기
+  $(".mobile-nav .gnb > li > a ").click(function () {
+    // console.log(this);
+    $(this).next(".mobile-nav .depth2").stop().slideToggle();
+  });
+  // 모바일용 메뉴 닫기 버튼
+  $("#close-menu").click(function () {
     $(".mobile-nav").animate(
       {
-        right: 0,
+        right: "-300px", //메뉴가 오른쪽 밖으로 나가도록함
       },
-      "-300px"
+      300
     );
   });
+  // 메뉴에 호버했을때
+  $("nav .gnb").hover(
+    function () {
+      // 마우스를 올렸을때
+      $("nav .depth2 , .header-bg").stop().slideDown();
+    },
+    function () {
+      // 마우스를 땟을때
+      $("nav .depth2 , .header-bg").stop().slideUp();
+    }
+  );
 });
